18075c9844cd31b6871aef91d09446db21810193
gibacache <g_ibacache@hotmail.com>
2016-07-11 15:31:45 -0400

    archivos si n seguimiento

---
+++ app/models/course.rb
@@ -0,0 +1,10 @@
+class Course < ActiveRecord::Base
+  has_many :trees
+  has_many :nodes, through: :trees
+  has_many :questionable, through: :nodes, source_type: 'ArgumentativeQuestion'
+
+
+  def to_label
+    "#{self.number} #{self.name}"
+  end
+end
---
+++ lib/tasks/tasks.rake
@@ -0,0 +1,6 @@
+task :db_reset do
+  Rake::Task['db:drop'].invoke
+  Rake::Task['db:create'].invoke
+  Rake::Task['db:migrate'].invoke
+  Rake::Task['db:seed'].invoke
+end
